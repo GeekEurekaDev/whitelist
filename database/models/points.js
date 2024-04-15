da70aae5c79951b3c1dff602130313660c3d6a9c
Ricky Rodrigo <rickyrodrigo.ph@gmail.com>
2024-04-15 12:11:25 +0800

    Delete database directory

--- database/models/points.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = new mongoose.Schema({
-    Guild: String,
-    User: String,
-    Points: Number
-});
-
-module.exports = mongoose.model("points", Schema);
